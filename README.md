2469da8eb831d21f53cb087607f2b604edb2a9b6
root <root@mundosangre.com>
2020-04-16 05:44:51 +0000

    Now rotates out the mysql dump from backupninja

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CoffeeBackup backup script for Coffee MUD (1.0)
+# CoffeeBackup backup script for Coffee MUD (1.1)
 Creates a backup of your Coffee MUD folder
 
 Official support sites: [Official Github Repo](https://github.com/fstltna/CoffeeBackup) - [Official Forum](https://pocketmud.com/index.php/forum/server-utils)  - [Official Download Area](https://pocketmud.com/index.php/download-upload/category/4-servers)
@@ -12,5 +12,7 @@ Official support sites: [Official Github Repo](https://github.com/fstltna/Coffee
         1 1 * * * /root/CoffeeBackup/coffeebackup.pl
 
 3. This will back up your Coffee MUD installation at 1:01am each day, and keep the last 5 backups.
+
+4. This assumes that backupninja is storing the daily backups to /root/backups and it rotates those sql dumps as well.
 
 If you need more help visit https://PocketMUD.com/
--- coffeebackup.pl
+++ coffeebackup.pl
@@ -3,12 +3,13 @@
 # Set these for your situation
 my $MTDIR = "/root/CoffeeMud";
 my $BACKUPDIR = "/root/backups";
+my $SQLDUMPDIR = "$BACKUPDIR/sqldump/";
 my $TARCMD = "/bin/tar czf";
 
 #-------------------
 # No changes below here...
 #-------------------
-my $VERSION = "1.0";
+my $VERSION = "1.1";
 
 print "CoffeeBackup.pl version $VERSION\n";
 print "========================\n";
@@ -41,5 +42,30 @@ if (-f "$BACKUPDIR/coffeebackup-1.tgz")
 }
 print "Done\nCreating New Backup: ";
 system("$TARCMD $BACKUPDIR/coffeebackup-1.tgz $MTDIR");
+print "Done\nMoving Existing MySQL data: ";
+if (-f "$SQLDUMPDIR/coffeemud.sql-5.gz")
+{
+	unlink("$SQLDUMPDIR/coffeemud.sql-5.gz")  or warn "Could not unlink $SQLDUMPDIR/coffeeimud.sql-5.gz: $!";
+}
+if (-f "$SQLDUMPDIR/coffeemud.sql-4.gz")
+{
+	rename("$SQLDUMPDIR/coffeemud.sql-4.gz", "$SQLDUMPDIR/coffeemud.sql-5.gz");
+}
+if (-f "$SQLDUMPDIR/coffeemud.sql-3.gz")
+{
+	rename("$SQLDUMPDIR/coffeemud.sql-3.gz", "$SQLDUMPDIR/coffeemud.sql-4.gz");
+}
+if (-f "$SQLDUMPDIR/coffeemud.sql-2.gz")
+{
+	rename("$SQLDUMPDIR/coffeemud.sql-2.gz", "$SQLDUMPDIR/coffeemud.sql-3.gz");
+}
+if (-f "$SQLDUMPDIR/coffeemud.sql-1.gz")
+{
+	rename("$SQLDUMPDIR/coffeemud.sql-1.gz", "$SQLDUMPDIR/coffeemud.sql-2.gz");
+}
+if (-f "$SQLDUMPDIR/coffeemud.sql.gz")
+{
+	rename("$SQLDUMPDIR/coffeemud.sql.gz", "$SQLDUMPDIR/coffeemud.sql-1.gz");
+}
 print("Done!\n");
 exit 0;
